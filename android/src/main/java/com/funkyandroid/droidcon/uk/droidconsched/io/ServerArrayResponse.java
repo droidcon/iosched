37d54d493ac68bb72e00d00b6c5214d8cf9640ba
Al Sutton <al@funkyandroid.com>
2013-09-11 14:18:45 +0100

    Switched to local service syncing

---
+++ android/src/main/java/com/funkyandroid/droidcon/uk/droidconsched/io/ServerArrayResponse.java
@@ -0,0 +1,13 @@
+package com.funkyandroid.droidcon.uk.droidconsched.io;
+
+import com.funkyandroid.droidcon.uk.droidconsched.io.ServerResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alsutton
+ * Date: 11/09/2013
+ * Time: 14:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerArrayResponse<T extends ServerResponse> extends ServerResponse {
+}
